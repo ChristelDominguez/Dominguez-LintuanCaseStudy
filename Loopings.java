e28180061fd8a819ca5649824779db822df6b11c
ChristelDominguez <79234979+ChristelDominguez@users.noreply.github.com>
2021-03-16 14:50:43 +0800

    Update Loopings.java

--- Loopings.java
+++ Loopings.java
@@ -4,6 +4,7 @@ public class Loopings {
     public static void main(String[] args) {
             int j;
        System.out.println("Lintuan, Danfer L.");
+        System.out.println("Dominguez, Christel Pauline R.");
        for(int i = 3; i<=15; i+=3){
            j = i*i;
            System.out.print(i + " ");
